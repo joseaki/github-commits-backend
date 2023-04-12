6c5244f9eae37da63d0e1ed1690cdc8039863eec
Antonio Alanya <joseakialanya@gmail.com>
2023-04-12 14:23:41 -0500

    feature/add_services: update main configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Github MS')
+    .setDescription('Microservice to get info from GitHub')
+    .setVersion('1.0')
+    .build();
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api/swagger', app, document);
+  await app.listen(8000);
 }
 bootstrap();
